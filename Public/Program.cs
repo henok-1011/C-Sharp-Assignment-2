db84faa8e4ffd0dee0f774bc1c59053dcc528156
henok-1011 <henniwizbekele@gmail.com>
2022-06-26 06:47:10 -1200

    Public folder added

---
+++ Public/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Welcome to age checker");
+bool shouldLoop = true;
+while (true)
+{
+    Console.WriteLine("Please enter age: ")
+    int age = Convert.ToInt32(Console.ReadLine());
+    if (age > 18)
+    {
+        Console.Write("Not old enough");
+    }
+    else
+    {
+        Console.WriteLine("Old enough");
+        shouldLoop = false;
+    }
+}
